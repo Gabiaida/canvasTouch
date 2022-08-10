e372fb7986ab84207c6cfeaa0cc0ee1a4f0f3d9a
Gabiaida <53183226+Gabiaida@users.noreply.github.com>
2022-08-10 11:35:49 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,65 @@
-canvas=document.getElementById("myCanvas");
+var lastPositionOfX, lastPositionOfY;
+color = "black";
+widthOfLine = 2;
 
-ctx=canvas.getContext("2d");
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
 
-function limpar(){
-ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+var width = screen.width;
+
+newWidth =  screen.width - 70; 
+newHeight = screen.height - 300;
+	if(width < 992)
+	{
+	document.getElementById("myCanvas").width = newWidth;
+    document.getElementById("myCanvas").height = newHeight;
+    document.body.style.overflow = "hidden";
+	}
+	canvas.addEventListener("touchstart", myTouchStart);
+
+function myTouchStart(e) 
+{
+	console.log("myTouchStart");
+ //Início da Atividade Adicional
+  color = document.getElementById("color").value;
+  widthOfLine = document.getElementById("widthOfLine").value;
+  
+//Fim da Atividade Adicional
+    lastPositionOfX = e.touches[0].clientX - canvas.offsetLeft;
+    lastPositionOfY = e.touches[0].clientY - canvas.offsetTop;
 }
 
-var largura=screen.width;
-var altura=screen.height;
-var Nlargura= largura-70;
-var Naltura= altura-300;
+canvas.addEventListener("touchmove", myTouchMove);
 
-if(largura<992){
-    document.getElementById("myCanvas").width= Nlargura;
-    document.getElementById("myCanvas").height=Naltura;
-    document.body.style.overflow="hidden";
-}
+function myTouchMove(e) 
+{
 
-canvas.addEventListener("touchstart",myTouchStart);
-function myTouchStart(e){
-   lastpositionX=e.touches[0].clientX- canvas.offsetLeft;
-   lastpositionY=e.touches[0].clientY-canvas.offsetTop;
-}
+	console.log("myTouchMove");
+    currentPositionOfTouchX = e.touches[0].clientX - canvas.offsetLeft;
+    currentPositionOfTouchY = e.touches[0].clientY - canvas.offsetTop;
 
-canvas.addEventListener("touchmove",myTouchMove);
-function myTouchMove(e){
-    positionX=e.touches[0].clientX- canvas.offsetLeft;
-    positionY=e.touches[0].clientY- canvas.offsetTop;
+    // mesmo código utilizado para a versão web
     ctx.beginPath();
-    ctx.strokeStyle=red;
-    ctx.lineWidth=2;
-    ctx.moveTo(lastpositionX, lastpositionY);
-    ctx.lineTo(positionX, positionY);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = widthOfLine;
+
+    console.log("Última posição das coordenadas x e y = ");
+    console.log("x = " + lastPositionOfX + "y = " + lastPositionOfY);
+    ctx.moveTo(lastPositionOfX, lastPositionOfY);
+
+    console.log("Posição atual das coordenadas x e y = ");
+    console.log("x  = " + currentPositionOfTouchX + "y = " + currentPositionOfTouchY);
+    ctx.lineTo(currentPositionOfTouchX, currentPositionOfTouchY);
     ctx.stroke();
-    lastpositionX=positionX;
-    lastpositionY=positionY;
+
+    lastPositionOfX = currentPositionOfTouchX; 
+    lastPositionOfY = currentPositionOfTouchY;
+    
+    // fim do mesmo código utilizado para a versão web
 }
+
+    // mesmo código utilizado para a versão web
+function clearArea() {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+    // fim do mesmo código utilizado para a versão web
